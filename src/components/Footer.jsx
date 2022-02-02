79dc70c4a9f7ca005fd383bd4f33ef2b40b16f44
Fernando del Valle <fernandomatiasdv@gmail.com>
2022-02-02 18:30:34 -0300

    feat: agregue la configuracion de jest y resolvi algunos warnings de las fuentes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from '../../styles/Home.module.css'
+
+const Footer = () =>
+    <footer className={styles.footer}>
+        <a
+            href="https://www.linkedin.com/in/fernando-del-valle/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Powered by Fernando del Valle
+        </a>
+    </footer>
+
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Footer from "components/Footer"
 import styles from '../../styles/Home.module.css'
 
 export default function Home() {
@@ -9,22 +9,11 @@ export default function Home() {
         <title>Sooft Pokémon</title>
         <meta name="Sooft Pokémon" content="Página generada por Fernando del Valle" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/fonts/PokemonGb-RAeo/PokemonGb-RAeo.ttf" as="font" crossOrigin=""/>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+
       </Head>
 
+      <Footer />
       
-      <footer className={styles.footer}>
-        <a
-          href="https://www.linkedin.com/in/fernando-del-valle/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Fernando del Valle
-        </a>
-      </footer>
     </div>
   )
 }
